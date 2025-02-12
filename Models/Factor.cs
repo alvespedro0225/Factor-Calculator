94b8a56da0587292193faeb0686bc42edecb2b6f
Pedro <alvespedro0225@gmail.com>
2025-02-12 18:58:27 -0300

    optimized memory usage

--- Models/Factor.cs
+++ Models/Factor.cs
@@ -8,57 +8,61 @@ public class Factor(NpgsqlConnection database)
 
     public int[] FindFactors(int target)
     {
-        return IsPrime(target, out var first, out var beginning) ?
-            [1, target] :
-            Helper(target, first, beginning);
-
-
-        bool IsPrime(int number, out int firstFactor, out int start)
+        // If the number isn't a prime, it will be divisible by a prime number smaller
+        // than or equal to it's square root. So in order to avoid allocating more memory
+        // for storing more primes, it's better to call the function many times, as it isn't resource intensive.
+        List<int> factors = [];
+        
+        while (!IsPrime(ref target, out var factor))
         {
-            var sqrt = (int) Math.Sqrt(target);
+            target /= factor;
+            factors.Add(factor);
+        }
+        if (factors.Count == 0) factors.Add(1);
+        factors.Add(target);
+        return factors.ToArray();
+        
+        bool IsPrime(ref int number, out int factor)
+        {
+            if (number == int.MaxValue)
+            {
+                factor = 0;
+                return true;
+            }
+            
+            var sqrt = (int) Math.Sqrt(number);
+            
             for (var i = 0; _primes[i] <= sqrt; i++)
             {
                 var currentPrime = _primes[i];
                 if (number % currentPrime != 0) continue;
-                firstFactor = currentPrime;
-                start = i;
+                factor = currentPrime;
                 return false;
             }
-            firstFactor = -1;
-            start = -1;
+            
+            if (factors.Count == 0) factors.Add(1);
+            factor = 0;
+            
             return true;
         }
 
-        int[] Helper(int number, int firstFactor, int start)
-        {
-            List<int> factors = [firstFactor];
-            number /= firstFactor;
-            while (number > 1)
-            {
-                if (number % _primes[start] != 0)
-                {
-                    start++;
-                    continue;
-                }
-                factors.Add(_primes[start]);
-                number /= _primes[start];
-            }
-            return factors.ToArray();
-        }
+
     }
 
     private static int[] GetPrimes(NpgsqlConnection connection)
     {
-        var primes = new int[105_097_565];
-        using var cmd = new NpgsqlCommand("SELECT * FROM primes ORDER BY number", connection);
+        var primes = new int[4792];
+        using var cmd = new NpgsqlCommand(
+            $"SELECT * FROM primes WHERE number < {(int) Math.Sqrt(int.MaxValue)} ORDER BY number",
+            connection
+        );
         using var reader = cmd.ExecuteReader();
         var i = 0;
         while (reader.Read())
         {
             primes[i++] = reader.GetInt32(0);
         }
-        Console.WriteLine($"{primes.Length}, {primes[0]}, {primes[1]}");
         connection.Close();
-        return primes;
+        return primes.ToArray();
     }
 }
